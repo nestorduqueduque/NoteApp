7a8c06801935ec3ec2ac37b37e9529d48d57bec1
nestorduqueduque <nestorduqueduque@gmail.com>
2024-01-24 22:04:14 -0500

    cors origin

--- backend/src/main/java/duquedev/noteapp/NoteappApplication.java
+++ backend/src/main/java/duquedev/noteapp/NoteappApplication.java
@@ -2,6 +2,9 @@ package duquedev.noteapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NoteappApplication {
@@ -10,6 +13,17 @@ public class NoteappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteappApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins
+						("https://note-app-iota-seven.vercel.app/").
+						allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
