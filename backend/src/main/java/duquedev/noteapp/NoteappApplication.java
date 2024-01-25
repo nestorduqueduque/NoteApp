30a83ee654d2aabdd33c776fde8401c417ac9227
nestorduqueduque <nestorduqueduque@gmail.com>
2024-01-24 23:16:29 -0500

    WebMvcConfig4

--- backend/src/main/java/duquedev/noteapp/NoteappApplication.java
+++ backend/src/main/java/duquedev/noteapp/NoteappApplication.java
@@ -2,6 +2,9 @@ package duquedev.noteapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,6 +14,15 @@ public class NoteappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteappApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
 	}
 
 
