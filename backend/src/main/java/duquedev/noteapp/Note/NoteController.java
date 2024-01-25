796e791408d16da59a1c97458b813dee24cb8372
nestorduqueduque <nestorduqueduque@gmail.com>
2024-01-24 22:24:21 -0500

    cors origin3

--- backend/src/main/java/duquedev/noteapp/Note/NoteController.java
+++ backend/src/main/java/duquedev/noteapp/Note/NoteController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("notes")
-@CrossOrigin(origins = "https://note-app-iota-seven.vercel.app")
+@CrossOrigin(origins = "https://note-app-iota-seven.vercel.app/notes")
 public class NoteController {
 
     @Autowired
